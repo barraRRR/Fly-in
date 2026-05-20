7262a06d43f82a353350ebe8e45604dd85a48e66
Javi Barreiro <235066522+barraRRR@users.noreply.github.com>
2026-05-20 11:08:01 +0200

    refactor: update simulator initialization and pathfinding logic for improved clarity and structure

--- class_simulator.py
+++ class_simulator.py
@@ -1,4 +1,5 @@
 from class_network import Network, Hub, Drone, DroneStatus, Zone
+from class_parser import MapParser
 from enum import Enum
 from collections import deque
 
@@ -7,21 +8,23 @@ class Simulatior:
     """
     """
     def __init__(self, map: str) -> None:
-        self.net = Network.parser(map)
-        self.net.connect_links()
-        self.net.start_drones()
+        self.map = MapParser(map)
+        self.net = Network(**self.map.data)
     
     def find_linear_path(self) -> deque:
         """
         """
-        current_hub = self.net.start_hub
-        turns = 0
-        while current_hub != self.net.end_hub:
+        total_paths = []
+        while True:
+            current_hub = self.net.start_hub
+            path = deque()
+            while current_hub != self.net.end_hub:
+                path.append(current_hub)
 
-        for hub in self.net.hub:
-            visited = []
-            for link in hub.links:
-                for edge, max in link.items():
+            for hub in self.net.hub:
+                visited = []
+                for link in hub.links:
+                    for edge, max in link.items():
 
     
     def simulate_turn(self) -> None:
